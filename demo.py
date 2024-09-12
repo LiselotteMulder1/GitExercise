30cfab67f52012169d8739fd5f8220dfd90364c8
LiselotteMulder1 <mulder.liselotteh@gmail.com>
2024-09-12 13:58:33 +0200

    First commit

---
+++ demo.py
@@ -0,0 +1,23 @@
+print('Rise & shine')
+a = 10
+b = 5
+
+class Dog:
+
+    def __init__(self, name):
+        self.name = name
+        self.tricks = []    # creates a new empty list for each dog
+        self.foods = []    # creates a new empty list for each dog
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
+    
+    def add_food(self, food):
+        self.foods.append(food)
+        
+d = Dog('Sara')
+e = Dog('Malin')
+
+d.add_trick('roll')
+e.add_trick('sit')
+d.add_food('beef')
